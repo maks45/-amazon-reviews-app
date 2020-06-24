910f5ec2158977cb473b1ae5cb6cfffe13b54c57
maks45 <maksim.durov45@gmail.com>
2020-06-24 18:35:25 +0300

    first commit

---
+++ src/main/java/com/maks/durov/amazonreviewapp/AmazonReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.maks.durov.amazonreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maks/durov/amazonreviewapp/AmazonReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maks.durov.amazonreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
