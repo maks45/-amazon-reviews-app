9d7f9b9973f993584a9796905c1fd1b1db8693c8
maks45 <maksim.durov45@gmail.com>
2020-06-25 21:14:58 +0300

    added ReviewParserService tests

---
+++ src/test/java/com/maks/durov/amazonreviewapp/service/ReviewParserServiceTest.java
@@ -0,0 +1,10 @@
+package com.maks.durov.amazonreviewapp.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ReviewParserServiceTest {
+    ReviewParserService reviewParserService = new ReviewParserService();
+
+
+}
