7a590ff7df2c60a6bcca797a79fde0f476630826
MaksimDurov <maksim.durov45@gmail.com>
2020-06-26 14:16:44 +0300

    read data from file (#1)
    * first commit
    
    * added DataProcessingException , added FileService with readFile method
    
    * added DataProcessingException , added FileService with readFile method
    
    * added tests for FileService class
    
    * changed test for FileService class
    
    * added empty lines to index.html and FileServiceTest.java

--- src/main/java/com/maks/durov/amazonreviewapp/AmazonReviewApplication.java
+++ src/main/java/com/maks/durov/amazonreviewapp/AmazonReviewApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AmazonReviewAppApplication {
-
+public class AmazonReviewApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AmazonReviewAppApplication.class, args);
+        SpringApplication.run(AmazonReviewApplication.class, args);
     }
-
 }
