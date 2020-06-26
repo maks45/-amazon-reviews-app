19368e82cecc95e460b9b9d8a93b188547436b29
MaksimDurov <maksim.durov45@gmail.com>
2020-06-26 14:16:44 +0300

    read data from file (#1)
    * first commit
    
    * added DataProcessingException , added FileService with readFile method
    
    * added DataProcessingException , added FileService with readFile method
    
    * added tests for FileService class
    
    * changed test for FileService class
    
    * added empty lines to index.html and FileServiceTest.java

--- src/main/java/com/maks/durov/amazonreviewapp/ServletInitializer.java
+++ src/main/java/com/maks/durov/amazonreviewapp/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(AmazonReviewAppApplication.class);
+        return application.sources(AmazonReviewApplication.class);
     }
 
 }
