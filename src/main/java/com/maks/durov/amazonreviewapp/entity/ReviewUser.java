d822d3bd74fe68ae4b5b845ff4e1014ec2a6ea45
maks45 <maksim.durov45@gmail.com>
2020-07-07 20:47:59 +0300

    added entities

--- src/main/java/com/maks/durov/amazonreviewapp/entity/ReviewUser.java
+++ src/main/java/com/maks/durov/amazonreviewapp/entity/ReviewUser.java
@@ -1,4 +1,13 @@
 package com.maks.durov.amazonreviewapp.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "review_users")
 public class ReviewUser {
+    @Id
+    private String id;
+    private String name;
 }
