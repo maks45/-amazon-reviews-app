8f961a59a3d933bbad6406b4b9f1a8e81e63b70d
maks45 <maksim.durov45@gmail.com>
2020-07-07 20:58:00 +0300

    added services for entities

---
+++ src/main/java/com/maks/durov/amazonreviewapp/service/ReviewUserService.java
@@ -0,0 +1,27 @@
+package com.maks.durov.amazonreviewapp.service;
+
+import org.springframework.stereotype.Service;
+import com.maks.durov.amazonreviewapp.entity.ReviewUser;
+import com.maks.durov.amazonreviewapp.repository.ReviewUserRepository;
+import java.util.Set;
+
+@Service
+public class ReviewUserService {
+    private final ReviewUserRepository reviewUserRepository;
+
+    public ReviewUserService(ReviewUserRepository reviewUserRepository) {
+        this.reviewUserRepository = reviewUserRepository;
+    }
+
+    public ReviewUser getReviewUserById(String userId){
+        return reviewUserRepository.getOne(userId);
+    }
+
+    public ReviewUser saveReviewUser(ReviewUser reviewUser){
+        return reviewUserRepository.save(reviewUser);
+    }
+
+    public void saveAll(Set<ReviewUser> reviewUsers){
+        reviewUserRepository.saveAll(reviewUsers);
+    }
+}
