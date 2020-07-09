7aedc0ad570a19f0e40725e30aac943540b74512
maks45 <maksim.durov45@gmail.com>
2020-07-09 17:21:58 +0300

    added controllers

--- src/main/java/com/maks/durov/amazonreviewapp/repository/ProductRepository.java
+++ src/main/java/com/maks/durov/amazonreviewapp/repository/ProductRepository.java
@@ -2,8 +2,12 @@ package com.maks.durov.amazonreviewapp.repository;
 
 import com.maks.durov.amazonreviewapp.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    List<Product> findProductsByReviewCount(int limit);
 }
