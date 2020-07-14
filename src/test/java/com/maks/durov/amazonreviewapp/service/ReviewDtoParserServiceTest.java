871d00d3bf73b037db61a865caa867ab6e0ec2a3
maks45 <maksim.durov45@gmail.com>
2020-07-14 16:19:47 +0300

    removed randundunt line in ReviewDtoParserTest

--- src/test/java/com/maks/durov/amazonreviewapp/service/ReviewDtoParserServiceTest.java
+++ src/test/java/com/maks/durov/amazonreviewapp/service/ReviewDtoParserServiceTest.java
@@ -27,6 +27,5 @@ class ReviewDtoParserServiceTest {
                 10, 1594136030L, "s,u,m,m,a,r,y", "t,e,x,t");
         ReviewDto actual = reviewDtoParserService.parseDto(csvLine);
         assertEquals(expected, actual);
-        int[] a = {1, 2, 03};
     }
 }
